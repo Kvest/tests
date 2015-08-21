5f4b779f81583106080895acdea1ec5279e1d827
Kvest <belokon.roman@gmail.com>
2015-08-21 19:03:14 +0300

    implemented TextInputLayout test

--- app/src/main/java/com/kvest/tests/ui/activity/MaterialTestsActivity.java
+++ app/src/main/java/com/kvest/tests/ui/activity/MaterialTestsActivity.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 import com.kvest.tests.R;
 
@@ -27,6 +30,22 @@ public class MaterialTestsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 TabLayoutTestActivity.start(MaterialTestsActivity.this);
+            }
+        });
+
+        findViewById(R.id.edit_text_email).setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    TextInputLayout textInputLayout = (TextInputLayout) findViewById(R.id.edit_text_email_wrapper);
+                    if (TextUtils.isEmpty(((EditText) view).getText())) {
+                        textInputLayout.setErrorEnabled(true);
+                        textInputLayout.setError("Email can't be null");
+                    } else {
+                        textInputLayout.setError(null);
+                        textInputLayout.setErrorEnabled(false);
+                    }
+                }
             }
         });
     }
