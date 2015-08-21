caa4fd0d909b3c77f349fa64e1e6ad59189b5550
Kvest <belokon.roman@gmail.com>
2015-08-21 21:19:20 +0300

    added Toolbar

--- app/src/main/java/com/kvest/tests/ui/activity/MaterialTestsActivity.java
+++ app/src/main/java/com/kvest/tests/ui/activity/MaterialTestsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
@@ -25,6 +26,9 @@ public class MaterialTestsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.material_tests_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         findViewById(R.id.tab_layout_test).setOnClickListener(new View.OnClickListener() {
             @Override
